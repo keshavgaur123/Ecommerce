b1815a4981d728e57d98fb225614c157d71e2bf2
unknown <https://github.com/keshavgaur123/Ecommerce>
2026-05-16 17:53:18 +0530

    some chanages in bashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Home from "./Page/Home";
 import ProductDetail from "./Page/ProductDetail";
@@ -9,16 +10,27 @@ import Dashboard from "./Page/Dashboard";
 
 function App() {
   return (
-    <div className="">
-      <Header />
+    <div className="flex">
+
+      {/* Sidebar (ONLY ONCE) */}
       <SideBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-       <Route path="/dashboard" element={<Dashboard />} />   
-      </Routes>
-      <SideBar />
-      <Footer />
+
+      {/* Main content area */}
+      <div className="flex-1 flex flex-col min-h-screen">
+
+        <Header />
+
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+
+        <Footer />
+
+      </div>
     </div>
   );
 }
--- src/Context/SideBarContext.jsx
+++ src/Context/SideBarContext.jsx
@@ -1,16 +1,25 @@
 import React, { useState, createContext } from "react";
+
 export const SideBarContext = createContext();
 
-function SideBarProvider({ children }) {
+const SideBarProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClose = () => {
     setIsOpen(false);
   };
+
+  const toggleSidebar = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <SideBarContext.Provider value={{ isOpen, setIsOpen, handleClose }}>
+    <SideBarContext.Provider
+      value={{ isOpen, setIsOpen, handleClose, toggleSidebar }}
+    >
       {children}
     </SideBarContext.Provider>
   );
-}
+};
+
 export default SideBarProvider;
